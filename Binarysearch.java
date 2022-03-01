2a1020386e357e9bf1769c2fa6d48150921395c5
unknown <filipdoringerkana@gmail.com>
2022-03-01 12:52:29 +0100

    Adding binarysearch class

---
+++ Binarysearch.java
@@ -0,0 +1,30 @@
+public class Binarysearch {
+    
+    /*
+    */
+    public int search(int[] A, int key){
+        
+        int l = 0;
+        int r = A.length;
+        int x;
+        do{
+            
+            x = (l + r) / 2;
+            
+            if( key < A[x]){
+                r = x;
+            }
+            else{
+                l = x + 1;
+            }
+
+        }while(key != A[x] && l < r);
+
+        if(key == A[x]){
+            return x;
+        }
+        else{
+            return -1;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -6,8 +6,9 @@ public class Main{
 
         Random rng = new Random();
         int[] test1 = new int[100];
+
         for(int i = 0; i < test1.length; i++){
-            test1[i] = rng.nextInt(10000);
+            test1[i] = rng.nextInt(100);
         }
         
         System.out.println("Unsorted:");
@@ -23,6 +24,21 @@ public class Main{
         for (int i : test1) {
             System.out.print(i);
             System.out.print(" ");
+        }
+
+        Binarysearch sorter = new Binarysearch();
+
+        int[] test2 = new int[3];
+        test2[0] = 1;
+        test2[1] = 3;
+        test2[2] = 5;
+        int foundindex = sorter.search(test2, 3);
+        
+        if(foundindex != -1){
+            System.out.println("Found");
+        }
+        else{
+            System.out.println("Did not find");
         }
 
     }
